911948eddaa1f8eadb23c92284dbc6639f3903d8
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-02 23:55:05 -0300

    add constant canvas width/height

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const TILE_SIZE = 256;
+export const CANVAS_HEIGHT = TILE_SIZE * 6;
+export const CANVAS_WIDTH = TILE_SIZE * 11;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 import * as Phaser from 'phaser';
 import GameScene from './scenes/GameScene';
-import SplashScene from './scenes/SplashScene';
-import SnowGame from './scenes/snow/SnowGame';
-import { TILE_SIZE } from './constants';
+import { CANVAS_HEIGHT, CANVAS_WIDTH} from './constants';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   pixelArt: true,
-  height: TILE_SIZE * (6 + 0),
-  width: TILE_SIZE * 11,
+  height: CANVAS_HEIGHT,
+  width: CANVAS_WIDTH,
   transparent: true,
   scale: {
     parent: 'game',
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  // scene: [SplashScene, LoadScene, GameScene],
   scene: [GameScene],
 };
 
