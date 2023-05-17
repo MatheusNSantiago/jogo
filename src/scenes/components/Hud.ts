3afee5c29740aca1e4a3b87179a16fad659bf16c
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-17 01:09:03 -0300

    feat(towers/hud): towers cant be dropped on top of the path

--- src/scenes/components/Hud.ts
+++ src/scenes/components/Hud.ts
@@ -1,9 +1,11 @@
-import { CANVAS_HEIGHT } from "../../constants";
-import Tower from "../../sprites/Tower";
-import Enemy from "../../sprites/enemy";
-import GameScene from "../GameScene";
+import { CANVAS_HEIGHT, PATH_LEVEL_1 } from '../../constants';
+import Tower from '../../sprites/Tower';
+import Enemy from '../../sprites/enemy';
+import GameScene from '../GameScene';
 
 class Hud extends Phaser.GameObjects.Container {
+  declare scene: GameScene;
+
   private dockButtonsCount = 0;
   private goldText: Phaser.GameObjects.Text;
   private healthBar: Phaser.GameObjects.Sprite;
@@ -23,10 +25,14 @@ class Hud extends Phaser.GameObjects.Container {
     this.addDockButton('archer-tower-card.png', 'archer-tower-front.png');
     this.addDockButton('castle-tower-card.png', 'castle-tower-front.png');
     this.addDockButton('knight-post-card.png', 'knight-post-front.png');
+    //
+    // this.addDockButton('power1.png', 'knight-post-front.png');
+    // this.addDockButton('power2.png', 'knight-post-front.png');
+    // this.addDockButton('power3.png', 'knight-post-front.png');
   }
 
   updateGold(gold: number) {
-    this.goldText.setText(gold.toString());
+    this.goldText.setText(gold.toString()).setDepth(100);
   }
 
   updateHealthBar(health: number) {
@@ -40,19 +46,31 @@ class Hud extends Phaser.GameObjects.Container {
   }
 
   private makeHealthBar(x: number, y: number) {
-    this.scene.add.sprite(x, y, 'hud', 'health.png').setOrigin(0,0);
-    return this.scene.add.sprite(x + 100, y + 20.5, 'hud', 'health-bar.png').setOrigin(0,0)
+    this.scene.add
+      .sprite(x, y, 'hud', 'health.png')
+      .setOrigin(0, 0)
+      .setDepth(100);
+    return this.scene.add
+      .sprite(x + 100, y + 20.5, 'hud', 'health-bar.png')
+      .setOrigin(0, 0)
+      .setDepth(101);
   }
 
-  private makeGoldCounter(initialGold: number, x: number, y:number) {
-    this.scene.add.sprite(x, y, 'hud', 'gold.png').setOrigin(0,0);
-    return this.scene.add.text(x + 65, y + 20, initialGold.toString(), {
-      fontSize: '44px',
-      color: '#fff',
-    });
+  private makeGoldCounter(initialGold: number, x: number, y: number) {
+    this.scene.add
+      .sprite(x, y, 'hud', 'gold.png')
+      .setOrigin(0, 0)
+      .setDepth(100);
+
+    return this.scene.add
+      .text(x + 65, y + 20, initialGold.toString(), {
+        fontSize: '44px',
+        color: '#fff',
+      })
+      .setDepth(101);
   }
 
-  private addDockButton(buttonFrame: string, towerFrame: string) {
+  private addDockButton(buttonFrame: string, frame: string) {
     const padding = 20 * this.dockButtonsCount;
     const dockButtonScale = 1.25;
     const dockButtonWidth = 171 * dockButtonScale;
@@ -71,19 +89,41 @@ class Hud extends Phaser.GameObjects.Container {
       .setInteractive({ cursor: 'grab' });
     this.scene.input.setDraggable(button);
 
+    // TODO: Refatorar essa parada para [Tower]
+    const isMouseOnTopOfPath = (x: number, y: number) => {
+      const path: Phaser.Curves.Path = this.scene.path;
+      const point = path.getPoints();
+      const distance = 85;
+      return point.some((p) => {
+        return Phaser.Math.Distance.Between(x, y, p.x, p.y) < distance;
+      });
+    };
+
     var tower: Tower;
     button.on('dragstart', ({ x, y }: Phaser.Input.Pointer) => {
-      tower = new Tower(this.scene, this.enemies, towerFrame, x, y);
+      tower = new Tower(this.scene, this.enemies, frame, x, y);
       tower.setInteractive({ cursor: 'grabbing' });
       this.scene.input.setDraggable(tower);
     });
     button.on('drag', ({ x, y }: Phaser.Input.Pointer) => {
+      if (isMouseOnTopOfPath(x, y)) {
+        this.scene.input.setDefaultCursor('not-allowed');
+      } else {
+        this.scene.input.setDefaultCursor('grabbing');
+      }
+
       tower.setPosition(x, y);
       tower.update();
     });
-    button.on('dragend', () => {
-      tower.enable();
-      this.towers.push(tower);
+    button.on('dragend', ({ x, y }: Phaser.Input.Pointer) => {
+      this.scene.input.setDefaultCursor('default');
+
+      if (isMouseOnTopOfPath(x, y)) {
+        tower.dispose();
+      } else {
+        tower.enable();
+        this.towers.push(tower);
+      }
     });
     this.dockButtonsCount++;
   }
