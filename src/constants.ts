5982b249dd7e9bd2ab5eac0cf6cd8e623647ef3f
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-18 09:08:39 -0300

    refactor: create ASSETS_PATH constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,9 @@
-import { EnemyConfig } from './sprites/enemy';
+import { EnemyConfig } from "./sprites/Enemy";
 
 export const TILE_SIZE = 256;
 export const CANVAS_HEIGHT = TILE_SIZE * 6;
 export const CANVAS_WIDTH = TILE_SIZE * 11;
+export const ASSETS_PATH = 'public/assets';
 
 export const PATH_LEVEL_1 = [
   { x: -50, y: 640 },
--- src/scenes/SplashScene.ts
+++ src/scenes/SplashScene.ts
@@ -1,3 +1,5 @@
+import { ASSETS_PATH } from "../constants";
+
 class SplashScene extends Phaser.Scene {
   constructor() {
     super({ key: 'SplashScene' });
@@ -5,49 +7,48 @@ class SplashScene extends Phaser.Scene {
 
   preload() {
     // Map
-    this.load.image("tiles", "src/assets/scenes/level1.png");
-    this.load.tilemapTiledJSON("level1", "src/assets/scenes/level1.json");
-
+    this.load.image("tiles", `${ASSETS_PATH}/scenes/level1.png`);
+    this.load.tilemapTiledJSON("level1", `${ASSETS_PATH}/scenes/level1.json`);
     // Enemies
     this.load.multiatlas(
       "enemy1",
-      "src/assets/sprites/enemy1/enemy1.json",
-      "src/assets/sprites/enemy1"
+      `${ASSETS_PATH}/sprites/enemy1/enemy1.json`,
+      `${ASSETS_PATH}/sprites/enemy1`
     );
-    this.load.json("enemy1-json","src/assets/sprites/enemy1/enemy1.json"),
+    this.load.json("enemy1-json",`${ASSETS_PATH}/sprites/enemy1/enemy1.json`),
 
     this.load.multiatlas(
       "enemy2",
-      "src/assets/sprites/enemy2/enemy2.json",
-      "src/assets/sprites/enemy2",
+      `${ASSETS_PATH}/sprites/enemy2/enemy2.json`,
+      `${ASSETS_PATH}/sprites/enemy2`,
     );
-    this.load.json("enemy2-json","src/assets/sprites/enemy2/enemy2.json"),
+    this.load.json("enemy2-json",`${ASSETS_PATH}/sprites/enemy2/enemy2.json`),
     this.load.multiatlas(
       "enemy3",
-      "src/assets/sprites/enemy3/enemy3.json",
-      "src/assets/sprites/enemy3",
+      `${ASSETS_PATH}/sprites/enemy3/enemy3.json`,
+      `${ASSETS_PATH}/sprites/enemy3`,
     );
-    this.load.json("enemy3-json","src/assets/sprites/enemy3/enemy3.json"),
+    this.load.json("enemy3-json",`${ASSETS_PATH}/sprites/enemy3/enemy3.json`),
 
     // dock
     this.load.atlas(
       "towers",
-      "src/assets/sprites/towers/towers.png",
-      "src/assets/sprites/towers/towers.json"
+      `${ASSETS_PATH}/sprites/towers/towers.png`,
+      `${ASSETS_PATH}/sprites/towers/towers.json`
     );
 
     // dock
     this.load.atlas(
       "dock",
-      "src/assets/sprites/gui/dock/dock.png",
-      "src/assets/sprites/gui/dock/dock.json"
+      `${ASSETS_PATH}/sprites/gui/dock/dock.png`,
+      `${ASSETS_PATH}/sprites/gui/dock/dock.json`
     );
 
     // hud
     this.load.atlas(
       "hud",
-      "src/assets/sprites/gui/hud/hud.png",
-      "src/assets/sprites/gui/hud/hud.json"
+      `${ASSETS_PATH}/sprites/gui/hud/hud.png`,
+      `${ASSETS_PATH}/sprites/gui/hud/hud.json`
     );
   }
 
