d908298cd288a3f3e0b8cbbe0ddacc6a129b6974
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-21 15:26:55 -0300

    feat(barrier): show hp when it has taken damage

--- src/sprites/Barrier.ts
+++ src/sprites/Barrier.ts
@@ -1,14 +1,16 @@
-import GameScene from '../scenes/GameScene';
-import Enemy from './Enemy';
-import HealthBar from './HealthBar';
+import GameScene from "../scenes/GameScene";
+import Enemy from "./Enemy";
+import HealthBar from "./HealthBar";
 
 class Barrier extends Phaser.GameObjects.Rectangle {
+  declare scene: GameScene;
   hp: HealthBar;
+  isHpVisible = false;
 
-  constructor(scene: GameScene, x: number, y: number, hp = 50) {
+  constructor(scene: GameScene, x: number, y: number, hp = 100) {
     super(scene, x, y, 200, 30, 0x663300);
     this.setDepth(2);
-    this.setActive(false)
+    this.setActive(false);
     this.hp = new HealthBar(scene, hp);
     this.hp.setVisible(false);
 
@@ -17,6 +19,15 @@ class Barrier extends Phaser.GameObjects.Rectangle {
 
   update() {
     this.hp.draw(this.x, this.y + 20);
+
+    // Se algum inimigo danificar a barreira, mostra o hp
+    if (!this.isHpVisible) {
+      const hasTakenDamage = this.hp.value < this.hp.total;
+      if (hasTakenDamage) {
+        this.isHpVisible = true;
+        this.hp.setVisible(true);
+      }
+    }
   }
 
   enable() {
@@ -25,22 +36,17 @@ class Barrier extends Phaser.GameObjects.Rectangle {
 
   hurt(damage: number) {
     this.hp.decrease(damage);
-    this.update()
+    this.update();
     if (this.hp.value == 0) this.dispose();
   }
 
   isCollidingWithEnemy(enemy: Enemy) {
-    if (!this.active) return false;
+    if (!this.active || !enemy.active) return false;
 
-    const enemyIsNearby = Phaser.Geom.Intersects.RectangleToRectangle(
+    return Phaser.Geom.Intersects.RectangleToRectangle(
       enemy.follower.getBounds(),
       this.getBounds()
     );
-
-    if (enemyIsNearby) this.hp.setVisible(true);
-    else this.hp.setVisible(false);
-
-    return enemyIsNearby;
   }
 
   dispose() {
@@ -51,18 +57,18 @@ class Barrier extends Phaser.GameObjects.Rectangle {
   static dragAndDrop(scene: GameScene, button: Phaser.GameObjects.Image) {
     var barrier: Barrier;
 
-    button.on('dragstart', ({ x, y }: Phaser.Input.Pointer) => {
-      barrier = new Barrier(scene, x, y).setInteractive({cursor: "pointer"});
+
+    button.on("dragstart", ({ x, y }: Phaser.Input.Pointer) => {
+      barrier = new this(scene, x, y).setInteractive({ cursor: "pointer" });
       scene.input.setDraggable(barrier);
     });
-    button.on('drag', ({ x, y }: Phaser.Input.Pointer) => {
-      scene.input.setDefaultCursor('grabbing');
+    button.on("drag", ({ x, y }: Phaser.Input.Pointer) => {
+      scene.input.setDefaultCursor("grabbing");
 
       barrier.setPosition(x, y);
-      barrier.update();
     });
-    button.on('dragend', () => {
-      scene.input.setDefaultCursor('default');
+    button.on("dragend", () => {
+      scene.input.setDefaultCursor("default");
       barrier.enable();
       scene.barrier = barrier;
     });
