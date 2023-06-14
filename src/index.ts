ad85745a64c08de651c7acd258a18c09ff945808
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-06-14 10:30:58 -0300

    feat: adicionar tela de vitoria

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,20 @@ import GameScene from './scenes/GameScene';
 import { CANVAS_HEIGHT, CANVAS_WIDTH} from './constants';
 import GameOverScene from './scenes/GameOverScene';
 import BootScene from './scenes/BootScene';
+import LevelCompleteScene from './scenes/LevelCompleteScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   height: CANVAS_HEIGHT,
   width: CANVAS_WIDTH,
-  fps: { limit: 35 },
+  fps: { limit: 25 },
   transparent: true,
   scale: {
     parent: 'game',
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [BootScene, GameScene, GameOverScene],
+  scene: [BootScene, GameScene, LevelCompleteScene, GameOverScene],
 };
 
 new Phaser.Game(config);
---
+++ src/scenes/LevelCompleteScene.ts
@@ -0,0 +1,27 @@
+class LevelCompleteScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'LevelCompleteScene' });
+  }
+
+  create() {
+    const { centerX, centerY } = this.cameras.main;
+    this.add
+      .text(centerX, centerY - 150, 'VOCÊ NÃO É UMA VERGONHA', {
+        color: '#0f0',
+        fontSize: 80,
+      })
+      .setOrigin(0.5);
+
+    const startButton = this.add
+      .text(centerX, centerY, 'Recomeçar', { fontSize: '52px', color: '#FFF' })
+      .setOrigin(0.5)
+      .setPadding(20)
+      .setStyle({ backgroundColor: '#111' })
+      .setInteractive({ useHandCursor: true })
+      .on('pointerdown', () => this.scene.start('GameScene'))
+      .on('pointerover', () => startButton.setStyle({ fill: '#f39c12' }))
+      .on('pointerout', () => startButton.setStyle({ fill: '#FFF' }));
+  }
+}
+
+export default LevelCompleteScene;
